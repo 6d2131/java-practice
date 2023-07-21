cf7325d6d51d858d65f6698511bb5c07545e9303
6d2131 <6d2131@gmail.com>
2023-07-22 04:21:59 +0900

    add

---
+++ 2/Variable.java
@@ -0,0 +1,10 @@
+class Variable {
+    public static void main(String[] args) {
+        int month;
+        month = 9;
+        int day = 16;
+    
+        System.out.print("このチャンネルは、");
+        System.out.print(month + "月" + day + "日に公開しました！");
+    }
+}
