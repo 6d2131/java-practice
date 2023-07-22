c4037bc4c1da3270371b300131d9a7e83969532b
6d2131 <6d2131@gmail.com>
2023-07-22 18:00:21 +0900

    add

---
+++ test1/ScoreArray.java
@@ -0,0 +1,11 @@
+class ScoreArray {
+    public static void main(String[] args) {
+        int[] score = {90, 80, 100, 90};
+
+        System.out.println("1人目は" + score[0] + "点");
+        System.out.println("2人目は" + score[1] + "点");
+        System.out.println("3人目は" + score[2] + "点");
+        System.out.println("4人目は" + score[3] + "点");
+
+    }
+}
