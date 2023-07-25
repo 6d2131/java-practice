e9f55f27f436143f383e6b6c4a6ac10300cc3bfc
6d2131 <6d2131@gmail.com>
2023-07-25 23:27:26 +0900

    add

--- 11/StuSample.java
+++ 11/StuSample.java
@@ -1,11 +1,16 @@
 class StuSample {
     public static void main(String[] args) {
         Student stu1 = new Student();
+        Student stu2 = new Student();
 
         stu1.name = "菅原";
         stu1.setScore(90, 80);
+        stu2.name = "田中";
+        stu2.setScore(70, 90);
 
         stu1.display();
         System.out.println("平均" + stu1.getAvg() + "点");
+        stu2.display();
+        System.out.println("平均" + stu2.getAvg() + "点");
     }
 }
